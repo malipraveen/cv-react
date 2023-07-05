9f8682fe6e864643895a12f5072608ef548c6bcf
franli <fei.li.best@gmail.com>
2023-07-05 22:13:17 +0800

    Update data.jsx

--- src/sections/data.jsx
+++ src/sections/data.jsx
@@ -1,6 +1,6 @@
 const LANG = 'CN'  // or EN
 
-const headlineEN = "Passionate about AI applications.";
+const headlineEN = "Msc in Data Science from Bocconi, former analyst at Agricultural Bank of China. Domain knowledge in deep learning, web development, finance and more. Eager to build AI applications.";
 
 const headlineCN = "数据科学硕士，农行总行两年全职工作经验，拥有深度学习、前端开发、经济金融等多个领域的知识，善于从数据中发现问题。求职方向：数据科学、AI落地应用。";
 
@@ -32,7 +32,7 @@ const infoEN = {
       url: "",
     },
   ],
-  language: ["Chinese (native)", "English (fluent)", "Italian (beginner)"],
+  language: ["Chinese (native)", "English (fluent)", "Italian (beginner)", "Japanese (beginner)"],
   techstack: [
     {
       icon: "python.png",
@@ -125,7 +125,7 @@ const infoCN = {
       url: "",
     },
   ],
-  language: ["中文 (母语)", "英语 (流利)", "意大利语 (入门)"],
+  language: ["中文 (母语)", "英语 (流利)", "意大利语 (入门)", "日语（入门）"],
   techstack: [
     {
       icon: "python.png",
@@ -196,32 +196,30 @@ const projectsEN = [
     name: "BeeNote",
     link: "https://beenote.app",
     year: 2023,
-    metric: {star:10, fork:1},
+    metric: {star:10, fork:3},
     description: [
-      "BeeNote is a full-stack CRUD application for taking language learning notes, built with Next.js 13 + Tailwind + DaisyUI + Tiptap + PocketBase + AWS SES + ChatGPT. It utilizes Next.js 13's latest app directory feature for routing.",
-      
-      "It features user registration, email sending, audio recording, file uploading, rich text editor, dark theme support, and mobile friendliness.",
+      "BeeNote is a full-stack CRUD AI application for taking language learning notes, built with Next.js 13 + Tailwind + PocketBase + AWS SES + ChatGPT, deployed on Vercel and fly.io. Users can ask AI for explaining words and grammars, edit notes and save their notes on the server.",
       // <span>Try it now at <a className="underline" href="https://beenote.app" target="_blank">https://beenote.app</a>.</span>
     ],
   },
-  {
-    icon: "pixel-weather.png",
-    name: "Pixel Weather",
-    link: "https://github.com/lifeitech/pixel-weather",
-    year: 2022,
-    metric: {star:1, fork:1},
-    description: [
-      "Pixel Weather is a mobile app built with React Native and Expo. It can ask the user's geographic location, fetch weather data from an API for that location, and display weather data with a concise UI. Different cities and also the theme can be saved on local devices.",
-    ],
-  },
+  // {
+  //   icon: "pixel-weather.png",
+  //   name: "Pixel Weather",
+  //   link: "https://github.com/lifeitech/pixel-weather",
+  //   year: 2022,
+  //   metric: {star:1, fork:1},
+  //   description: [
+  //     "Pixel Weather is a mobile app built with React Native and Expo. It can ask the user's geographic location, fetch weather data from an API for that location, and display weather data with a concise UI. Different cities and also the theme can be saved on local devices.",
+  //   ],
+  // },
   {
     icon: "github.svg",
-    name: "Noise Contrastive Estimation (NCE) in PyTorch",
+    name: "NCE in PyTorch",
     link: "https://github.com/lifeitech/nce",
     year: 2020,
     metric: {star:18, fork:3},
     description: [
-    "NCE is a method for estimating energy models in deep learning, by training a model to distinguish between data and noise through logistic regression.", 
+    "Noise Contrastive Estimation (NCE) is a method for estimating energy models in deep learning, by training a model to distinguish between data and noise through logistic regression.", 
   ],
   },
 ];
@@ -232,7 +230,7 @@ const projectsCN = [
     name: "BeeNote",
     link: "https://beenote.app",
     year: 2023,
-    metric: {star:10, fork:1},
+    metric: {star:10, fork:3},
     description: [
       "BeeNote 是一个用来记录外语学习笔记的全栈web应用。用户注册登录后，可以创建笔记本，保存字母、单词、语法等笔记。技术栈采用 Next.js 13 + TailwindCSS + PocketBase + AWS。本应用使用stream的方式接入了OpenAI的API，用户可利用ChatGPT极大提升外语学习的效率。",
       "已获得来自30多个国家用户的访问，用户数量正不断增长。"
@@ -255,22 +253,42 @@ const projectsCN = [
     year: 2020,
     metric: {star:18, fork:3},
     description: [
-    "Noise Contrastive Estimation (NCE) 是深度学习领域用来估计 energy model 参数的一个方法，其思想广泛用于word2vec、CLIP等众多模型。此方法将训练数据贴上正标签，将噪音贴上负标签，通过logistic regression进行数据和噪音的对比学习，从而将无监督学习问题转化为有监督学习问题。",
+    "Noise Contrastive Estimation (NCE) 是深度学习领域用来估计energy model参数的一个方法，其思想广泛用于word2vec、CLIP等众多模型。此方法将训练数据贴上正标签，将噪音贴上负标签，通过logistic regression进行数据和噪音的对比学习，从而将无监督学习问题转化为有监督学习问题。",
   ],
   },
 ];
 
 const experienceEN = [
   {
+    icon: "pingan.jpeg",
+    company: "Ping An Technology",
+    pos: "Product Manager Intern",
+    loc: "Shenzhen, China",
+    time: "2021, 2 mos",
+    description: [
+      "We offer our clients machine translation service for translating Chinese financial reports into English. We have a platform that lets outsourced workers manually verify and correct model outputs. By observing user data, I discovered that quite often workers resort to external services like Google Translate for their job. I proposed a way to detect such behavior."
+    ]
+  },
+  {
     icon: "transfereasy.png",
-    company: "transfereasy.com",
+    company: "TransferEasy",
     pos: "Data Intern",
     loc: "Beijing, China",
     time: "2018, 6 mos",
     description: [
-      "Scraped officials, crimes and enterprises data from the Internet, using Python Scrapy library. Developed a named entity recognition (NER) program for extracting person names from scraped data.",
+      "Utilizing the Python Scrapy library, I scraped massive government officials, crimes and enterprises data from the Internet, for building an anti-money laundering database product. I also developed a named entity recognition (NER) program for extracting Chinese names from scraped data.",
     ]
-  }
+  },
+  {
+    icon: "harvard.png",
+    company: "Harvard University",
+    pos: "Research Assistant",
+    loc: "Boston, USA",
+    time: "2017, 4 mos",
+    description: [
+      "Worked in a team to digitalize economic data from 2,000+ Chinese county gazetteers at Harvard Yenching Library. Used OCR software to convert tables on papers into spreadsheets, and performed data cleaning, merge, and analysis with Python."
+    ]
+  },
 ]
 
 const experienceCN = [
@@ -278,7 +296,7 @@ const experienceCN = [
     icon: "pingan.jpeg",
     company: "平安科技 Ping An Technology",
     pos: "产品经理实习",
-    loc: "北京, 中国",
+    loc: "深圳, 中国",
     time: "2021, 2个月",
     description: [
       "平安资管需要将国内上市公司财务报告等大量金融专业文档翻译成英文供国际投资者参考。我负责供数据标注人员使用的机器翻译校对平台产品，通过观察数据，成功发现了用户直接使用百度翻译等工具作弊的行为，并提出了解决方案。"
@@ -315,6 +333,7 @@ const educationEN = [
     time: "2018 ~ 2020, 2 yrs",
     description: [
       "GPA: 110/110 cum laude",
+      "Research: Generative Learning"
     ],
   },
   {
@@ -332,7 +351,7 @@ const educationCN = [
     icon: "bocconi.png",
     school: "博科尼大学 Università Bocconi",
     degree: "理学硕士，数据科学",
-    loc: "米兰, 意大利",
+    loc: "米兰，意大利",
     time: "2018 ~ 2020, 两年",
     description: [
       "GPA: 110/110 cum laude",
